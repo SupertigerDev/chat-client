cd10210bae05ee7cde93cf8b7ced9c413e624455
Supertiger <saawalkhn@googlemail.com>
2022-05-19 12:32:10 +0100

    fixed CustomSuspense

--- src/components/CustomSuspense/CustomSuspense.tsx
+++ src/components/CustomSuspense/CustomSuspense.tsx
@@ -1,5 +1,5 @@
 import { Suspense } from "preact/compat";
 
-export function CustomSuspense (props: {children: any}, fallback?: any) {
-  return <Suspense fallback={fallback || <div>Loading...</div>}>{props.children}</Suspense>
+export function CustomSuspense (props: {children: any, fallback?: any}) {
+  return <Suspense fallback={props.fallback || <div>Loading...</div>} {...props}/>
 }
